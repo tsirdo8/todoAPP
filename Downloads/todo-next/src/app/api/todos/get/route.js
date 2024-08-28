7fae4bd86b3c725242712fa9b3493ffa73c6fff3
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2024-08-28 04:07:08 +0400

    Delete Downloads/todo-next directory

--- Downloads/todo-next/src/app/api/todos/get/route.js
+++
@@ -1,8 +0,0 @@
-import { todos } from "@/services/mongo";
-
-export async function GET() {
-
-    const allTodos = await todos.find().toArray();
-    return new Response(JSON.stringify(allTodos));
-  
-}
--- Downloads/todo-next/src/services/mongo.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`;
-const client = new MongoClient(uri);
-const database = client.db("next-mongo");
-
-export const todos = database.collection("todos");
